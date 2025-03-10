ec8fc04cb977f1b41500737971ae15b6c2a9deb3
Talfaza <charifyahia4@gmail.com>
2025-03-10 20:56:33 +0000

    add: SSH Routes in main

--- backend/main.go
+++ backend/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/gofiber/fiber/v3"
-	"github.com/joho/godotenv"
+    "log"
+    "os"
+    "github.com/gofiber/fiber/v3"
+    "github.com/joho/godotenv"
+    "github.com/Talfaza/Netfixer/routes"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	port := os.Getenv("PORT")
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	app := fiber.New()
+    port := os.Getenv("PORT")
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Backend Server On Port : " + port)
-	})
+    app := fiber.New()
 
-	log.Fatal(app.Listen(":" + port))
-	if err != nil {
-		log.Fatal("Error Starting Server....")
-	}
+    app.Get("/", func(c fiber.Ctx) error {
+        return c.SendString("Backend Server On Port: " + port)
+    })
+
+    routes.RouteSetup(app)
+
+    log.Fatal(app.Listen(":" + port))
 }
