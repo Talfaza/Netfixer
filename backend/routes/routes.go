6a932b8c5ece21cc8890bf65c538af8a5d695388
Talfaza <charifyahia4@gmail.com>
2025-03-10 20:59:19 +0000

    add: SSH Routes in main

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,9 +2,9 @@ package routes
 
 import (
     "github.com/gofiber/fiber/v3"
-    "github.com/Talfaza/Netfixer/mircoservices"
+    "github.com/Talfaza/Netfixer/microservices"
 )
 
 func RouteSetup(app *fiber.App) {
-    app.Post("/api/execute", controllers.ExecuteCommand)
+    app.Post("/api/execute", microservices.ExecuteCommand)
 }
