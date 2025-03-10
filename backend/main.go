ca08982b07cd4cf4e6ba8aae353fc5d290488963
Talfaza <charifyahia4@gmail.com>
2025-03-10 20:41:18 +0000

    Revert "add: routing to the main"
    This reverts commit 671be84c4589307f9092a65ad1855219cd058f5c.

--- backend/main.go
+++ backend/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "log"
-    "os"
-    "github.com/gofiber/fiber/v3"
-    "github.com/joho/godotenv"
-    "github.com/Talfaza/Netfixer/routes"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 
-    port := os.Getenv("PORT")
+	app := fiber.New()
 
-    app := fiber.New()
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Backend Server On Port : " + port)
+	})
 
-    app.Get("/", func(c fiber.Ctx) error {
-        return c.SendString("Backend Server On Port: " + port)
-    })
-
-    routes.RouteSetup(app)
-
-    log.Fatal(app.Listen(":" + port))
+	log.Fatal(app.Listen(":" + port))
+	if err != nil {
+		log.Fatal("Error Starting Server....")
+	}
 }
